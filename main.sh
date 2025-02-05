0ec30e01e5be2d9a2c93c9861d2d706fa3517a97
MrAbbas <de3ktop@gmail.com>
2025-02-05 22:26:24 +0330

    main.sh

--- main.sh
+++ main.sh
@@ -43,7 +43,7 @@ install_dependencies() {
 clone_project() {
     echo "Cloning the project to $PROJECT_DIR..."
     sudo rm -rf "$PROJECT_DIR"
-    sudo git clone https://github.com/dev-ir/xui-subscription-template.git "$PROJECT_DIR"
+    sudo git clone https://github.com/ParsDev/xui-subscription-template.git "$PROJECT_DIR"
     cd "$PROJECT_DIR" || exit
 }
 
